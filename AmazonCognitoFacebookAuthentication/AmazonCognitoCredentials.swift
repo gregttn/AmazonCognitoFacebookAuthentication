f04b9f2af17eedf4fab1a577e3dcc1f61bc4fff0
gregttn <gregttn@gmail.com>
2015-01-03 16:28:19 +0000

    Struct to hold credentials to cognito

---
+++ AmazonCognitoFacebookAuthentication/AmazonCognitoCredentials.swift
@@ -0,0 +1,15 @@
+//
+//  AmazonCognitoCredentials.swift
+//  AmazonCognitoFacebookAuthentication
+//
+//  Created by Grzegorz Tatarzyn on 03/01/2015.
+//  Copyright (c) 2015 Grzegorz Tatarzyn. All rights reserved.
+//
+
+struct AmazonCognitoCredentials {
+    static let region: AWSRegionType = AWSRegionType.USEast1
+    static let poolId: String = "YOUR POOL ID"
+    static let unauthArn: String = "YOUR unauthArn"
+    static let authArn: String = "YOUR authArn"
+    static let accountId: String = "YOUR ACCOUNT ID"
+}
